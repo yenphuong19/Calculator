bc1be4db353f3d8abe8d6d5460e0131b69be0bed
yenphuong19 <yenphuong190996@gmail.com>
2022-08-15 22:58:13 +0700

    15/8/22

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App vh-100 bg-danger bg-gradient d-flex align-items-center">
+      <div className="container bg-secondary shadow rounded-5 d-flex flex-column justify-content-evenly">
+
+        <div className='result'>
+          <h1 style={{height: '50px'}}>Kết quả</h1>
+          <h3 style={{height: '40px'}}>Phép tính</h3>
+        </div>
+
+        <div className='row'>
+          <div className='col-9 p-0 d-flex flex-column'>
+            <div className='d-flex'>
+              <button className='btn btn-light shadow flex-grow-1'>Ccc</button>
+              <button className='btn btn-light shadow flex-grow-1'>+/-</button>
+              <button className='btn btn-light shadow flex-grow-1'>%</button>
+            </div>
+            <div className='d-flex'>
+              <button className='btn btn-dark shadow flex-grow-1'>7</button>
+              <button className='btn btn-dark shadow flex-grow-1'>8</button>
+              <button className='btn btn-dark shadow flex-grow-1'>9</button>
+            </div>
+            <div className='d-flex'>
+              <button className='btn btn-dark shadow flex-grow-1'>4</button>
+              <button className='btn btn-dark shadow flex-grow-1'>5</button>
+              <button className='btn btn-dark shadow flex-grow-1'>6</button>
+            </div>
+            <div className='d-flex'>
+              <button className='btn btn-dark shadow flex-grow-1'>1</button>
+              <button className='btn btn-dark shadow flex-grow-1'>2</button>
+              <button className='btn btn-dark shadow flex-grow-1'>3</button>
+            </div>
+            <div className='d-flex'>
+              <button className='btn btn-dark shadow flex-grow-1'>0</button>
+              <button className='btn btn-dark shadow flex-grow-1'>.</button>
+            </div>
+          </div>
+          <div className='col-3 p-0 d-flex flex-column'>
+            <button className='btn btn-warning shadow'>/</button>
+            <button className='btn btn-warning shadow'>x</button>
+            <button className='btn btn-warning shadow'>-</button>
+            <button className='btn btn-warning shadow'>+</button>
+            <button className='btn btn-danger shadow'>=</button>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
