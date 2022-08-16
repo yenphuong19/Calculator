b8c56d03d2feee7fb8eeaf6218baed69ea7172d3
yenphuong19 <96897672+yenphuong19@users.noreply.github.com>
2022-08-16 11:46:23 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   const [result, setResult] = useState();
   const [cal, setCal] = useState();
 
-  const handleIncrement = (e) => {
-    console.log(e.target.value);
-    setCal((cal) => cal + e.target.value);
+  console.log(cal);
+  const handleChangeExpression = (e) => {
+    setCal((cal) => (cal ? cal + e.target.value : e.target.value));
   };
+
+  const handleReset = () => setCal();
   return (
     <div className="App vh-100 bg-danger bg-gradient d-flex align-items-center">
       <div className="container bg-secondary shadow rounded-5 d-flex flex-column justify-content-evenly">
@@ -20,7 +22,12 @@ function App() {
         <div className="row">
           <div className="col-9 p-0 d-flex flex-column">
             <div className="d-flex">
-              <button className="btn btn-light shadow flex-grow-1">Ccc</button>
+              <button
+                className="btn btn-light shadow flex-grow-1"
+                onClick={handleReset}
+              >
+                C
+              </button>
               <button className="btn btn-light shadow flex-grow-1">+/-</button>
               <button className="btn btn-light shadow flex-grow-1">%</button>
             </div>
@@ -28,21 +35,21 @@ function App() {
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={7}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 7
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={8}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 8
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={9}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 9
               </button>
@@ -51,21 +58,21 @@ function App() {
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={4}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 4
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={5}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 5
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={6}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 6
               </button>
@@ -74,21 +81,21 @@ function App() {
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={1}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 1
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={2}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 2
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={3}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 3
               </button>
@@ -97,14 +104,14 @@ function App() {
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value={0}
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 0
               </button>
               <button
                 className="btn btn-dark shadow flex-grow-1"
                 value="."
-                onClick={handleIncrement}
+                onClick={handleChangeExpression}
               >
                 .
               </button>
@@ -114,7 +121,13 @@ function App() {
             <button className="btn btn-warning shadow">/</button>
             <button className="btn btn-warning shadow">x</button>
             <button className="btn btn-warning shadow">-</button>
-            <button className="btn btn-warning shadow">+</button>
+            <button
+              className="btn btn-warning shadow"
+              value="+"
+              onClick={handleChangeExpression}
+            >
+              +
+            </button>
             <button className="btn btn-danger shadow">=</button>
           </div>
         </div>
