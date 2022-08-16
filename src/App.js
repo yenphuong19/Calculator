7eba96e6cb87f8bad8e11d9f2d4c59f4cec5e4dd
yenphuong19 <96897672+yenphuong19@users.noreply.github.com>
2022-08-16 10:04:58 +0700

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import "./App.css";
 function App() {
   const [result, setResult] = useState();
   const [cal, setCal] = useState();
+
+  const handleIncrement = (e) => {
+    console.log(e.target.value);
+    setCal((cal) => cal + e.target.value);
+  };
   return (
     <div className="App vh-100 bg-danger bg-gradient d-flex align-items-center">
       <div className="container bg-secondary shadow rounded-5 d-flex flex-column justify-content-evenly">
@@ -20,23 +25,89 @@ function App() {
               <button className="btn btn-light shadow flex-grow-1">%</button>
             </div>
             <div className="d-flex">
-              <button className="btn btn-dark shadow flex-grow-1">7</button>
-              <button className="btn btn-dark shadow flex-grow-1">8</button>
-              <button className="btn btn-dark shadow flex-grow-1">9</button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={7}
+                onClick={handleIncrement}
+              >
+                7
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={8}
+                onClick={handleIncrement}
+              >
+                8
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={9}
+                onClick={handleIncrement}
+              >
+                9
+              </button>
             </div>
             <div className="d-flex">
-              <button className="btn btn-dark shadow flex-grow-1">4</button>
-              <button className="btn btn-dark shadow flex-grow-1">5</button>
-              <button className="btn btn-dark shadow flex-grow-1">6</button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={4}
+                onClick={handleIncrement}
+              >
+                4
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={5}
+                onClick={handleIncrement}
+              >
+                5
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={6}
+                onClick={handleIncrement}
+              >
+                6
+              </button>
             </div>
             <div className="d-flex">
-              <button className="btn btn-dark shadow flex-grow-1">1</button>
-              <button className="btn btn-dark shadow flex-grow-1">2</button>
-              <button className="btn btn-dark shadow flex-grow-1">3</button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={1}
+                onClick={handleIncrement}
+              >
+                1
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={2}
+                onClick={handleIncrement}
+              >
+                2
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={3}
+                onClick={handleIncrement}
+              >
+                3
+              </button>
             </div>
             <div className="d-flex">
-              <button className="btn btn-dark shadow flex-grow-1">0</button>
-              <button className="btn btn-dark shadow flex-grow-1">.</button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value={0}
+                onClick={handleIncrement}
+              >
+                0
+              </button>
+              <button
+                className="btn btn-dark shadow flex-grow-1"
+                value="."
+                onClick={handleIncrement}
+              >
+                .
+              </button>
             </div>
           </div>
           <div className="col-3 p-0 d-flex flex-column">
